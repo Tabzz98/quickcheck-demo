eadb2908d7cb2498d5f66243c1aa94c6cec35ecf
Tabzz98 <muhammadtabaza@gmail.com>
2019-03-01 17:50:27 +0200

    Added extra sources to README, and modified test properties.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # quickcheck-demo
 
 This is a demostration of property-based testing in Haskell.
+
+You can read more about property-based testing and Haskell here:
+    * https://www.leadingagile.com/2018/04/step-by-step-toward-property-based-testing/
+        - A useful explanation of property-based testing with an introduction to JUnit Quick Check.
+    * http://www.cse.chalmers.se/~rjmh/QuickCheck/manual.html
+        - QuickCheck documentation.
+    * http://learnyouahaskell.com/chapters
+        - A good and simple way to learn Haskell programming.
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,22 +1,23 @@
 import Test.QuickCheck
 import Utils
 
-prop_reverse_of_reverse_eq_identity xs = (reverseList $ reverseList xs) == xs
+prop_reverseOfReverseEqIdentity xs = (reverseList $ reverseList xs) == xs
 
-prop_inserted_element_is_smaller_than_its_successor x ys =
-  let whole = insertInOrder x ys
-   in case dropWhile (\e -> e /= x) whole of
-        (a:b:cs) -> a <= b
-        _ -> True
-
-prop_sorted_list_cannot_be_further_sorted xs =
+prop_sortedListCannotBeFurtherSorted xs =
   insertionSort xs == (insertionSort $ insertionSort xs)
+
+prop_allPredecessorsLtSuccessors xs = isSorted (insertionSort xs)
+  where
+    isSorted (x:y:xs) = x <= y && isSorted xs
+    isSorted _ = True
 
 main = do
   putStrLn "Testing reverseList:"
-  quickCheckResult (prop_reverse_of_reverse_eq_identity :: [Int] -> Bool)
-  putStrLn "Testing insertInOrder:"
-  quickCheckResult
-    (prop_inserted_element_is_smaller_than_its_successor :: Int -> [Int] -> Bool)
+  quickCheckWith
+    (stdArgs {maxSuccess = 5000})
+    (prop_reverseOfReverseEqIdentity :: [String] -> Bool)
   putStrLn "Testing insertionSort:"
-  quickCheckResult (prop_sorted_list_cannot_be_further_sorted :: [Int] -> Bool)
+  quickCheckWith
+    (stdArgs {maxSuccess = 10000})
+    (prop_sortedListCannotBeFurtherSorted :: [Int] -> Bool)
+  quickCheck (prop_allPredecessorsLtSuccessors :: [Int] -> Bool)
